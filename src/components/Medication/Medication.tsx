a65a085eaacebc4b473de4d318f156fd2661b524
Olga Tsybikova <tsy2701@gmail.com>
2022-06-12 21:13:16 +0200

    Fixed medication Form function

--- src/components/Medication/Medication.tsx
+++ src/components/Medication/Medication.tsx
@@ -16,9 +16,7 @@ export type MedicationProps = {
 const Medication = ({
   title,
   image,
-  prospect,
-  uses,
-  treatment,
+  category,
   id,
 }: MedicationProps): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -33,7 +31,7 @@ const Medication = ({
   return (
     <li className="container">
       <div className="py-6 px-8">
-        <div className="flex max-w-md shadow-lg rounded-lg mx-auto bg-white overflow-hidden md:max-w-2xl">
+        <div className="flex max-w-md shadow-lg rounded-lg mx-auto bg-white border border-gray-300 overflow-hidden md:max-w-2xl">
           <div className="w-1/3 bg-cover">
             <img
               src={`${process.env.REACT_APP_API_URL}images/${image}`}
@@ -42,28 +40,22 @@ const Medication = ({
             ></img>
           </div>
           <div className="w-2/3 p-4">
-            <h1 className="text-gray-900 font-bold text-2xl">{title}</h1>
-            <p className="mt-2 text-gray-600 text-sm">{uses}</p>
-            <a href={prospect} className="text-1 text-cyan-800 font-medium">
-              Read Prospect here
-            </a>
-            <div className="flex item-center justify-between mt-3">
-              <h1 className="text-gray-700 font-regular text-xs">
-                {treatment}
-              </h1>
-              <button
-                onClick={navigateToEdit}
-                className="button bg-emerald-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg border-2"
-              >
-                Edit
-              </button>
-              <button
-                onClick={handleDelete}
-                className="button bg-emerald-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg border-2"
-              >
-                Delete
-              </button>
-            </div>
+            <h1 className="text-gray-800 font-semibold text-xl capitalize">
+              {title}
+            </h1>
+            <p className="mt-2 text-gray-600 text-sm">Category: {category}</p>
+            <button
+              onClick={navigateToEdit}
+              className="button bg-emerald-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg border-2"
+            >
+              Edit
+            </button>
+            <button
+              onClick={handleDelete}
+              className="button bg-emerald-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg border-2"
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
