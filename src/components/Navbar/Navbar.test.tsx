2af25caf39f211db1e681f54a98ba901a2477069
Olga Tsybikova <tsy2701@gmail.com>
2022-06-15 11:02:13 +0200

    Tested navbar logout

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,4 +1,7 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../redux/store/store";
@@ -20,6 +23,67 @@ describe("Given a Navbar component function", () => {
       const navbar = screen.getAllByRole("img", { name: "logo" });
 
       expect(navbar.length).toEqual(expectedNumberOfElements);
+    });
+    describe("When its invoked and the status is not logged", () => {
+      test("Then it should renderize 3 links and one button", () => {
+        const expectedLinks = 3;
+        let mockLogged = false;
+        const mockUserSlice = createSlice({
+          name: "user",
+          initialState: { logged: mockLogged },
+          reducers: {},
+        });
+
+        const mockStore = configureStore({
+          reducer: { user: mockUserSlice.reducer },
+        });
+
+        render(
+          <Provider store={mockStore}>
+            <BrowserRouter>
+              <Navbar />
+            </BrowserRouter>
+          </Provider>
+        );
+
+        const menuButton = screen.getByRole("button", { name: "menu" });
+        const searchButton = screen.getByRole("button", { name: "search" });
+        const totalLinks = screen.getAllByRole("link");
+        const registerLink = screen.getByRole("link", { name: "Login" });
+        const loginLink = screen.getByRole("link", { name: "Register" });
+        userEvent.click(menuButton);
+        userEvent.click(searchButton);
+
+        expect(registerLink).toBeInTheDocument();
+        expect(loginLink).toBeInTheDocument();
+        expect(totalLinks).toHaveLength(expectedLinks);
+      });
+    });
+    describe("when the text Logout is clicked on", () => {
+      test("Then it should dispatch a logout action", async () => {
+        let mockLogged = true;
+        const mockUserSlice = createSlice({
+          name: "user",
+          initialState: { logged: mockLogged },
+          reducers: {},
+        });
+
+        const mockStore = configureStore({
+          reducer: { user: mockUserSlice.reducer },
+        });
+
+        render(
+          <Provider store={mockStore}>
+            <BrowserRouter>
+              <Navbar />
+            </BrowserRouter>
+          </Provider>
+        );
+
+        const logoutLink = screen.getByRole("link", { name: "Logout" });
+        userEvent.click(logoutLink);
+        expect(logoutLink).toBeInTheDocument();
+      });
     });
   });
 });
