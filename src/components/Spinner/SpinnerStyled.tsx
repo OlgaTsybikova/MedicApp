36d7dd098cb39373ecea9251e378ee7d44b09327
Olga Tsybikova <tsy2701@gmail.com>
2022-06-28 15:01:44 +0200

    Centerred spinner

--- src/components/Spinner/SpinnerStyled.tsx
+++ src/components/Spinner/SpinnerStyled.tsx
@@ -2,18 +2,14 @@ import styled from "styled-components";
 
 const SpinnerStyled = styled.div`
   z-index: 100;
+  position: sticky;
   display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  position: fixed;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
+  justify-content: center;
+  align-content: center;
   color: #fdfffc;
-  opacity: 0.65;
+  opacity: 0.75;
 
   span {
-    margin-top: 35px;
     font-size: 20px;
     font-weight: 500;
     user-select: none;
